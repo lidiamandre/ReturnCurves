853e4d303ff6f249925f4544ddada03836ed2b09
Andre <andrel@lancaster.ac.uk>
2024-05-02 11:16:56 +0100

    Changed documentation

--- R/returncurve.R
+++ R/returncurve.R
@@ -12,13 +12,12 @@
 #' @param w sequence of angles between 0 and 1; default set to a vector of 101 equally spaced angles 
 #' @param method method to be used in the estimation of the angular dependence function: "hill" to use the Hill estimator, "cl" for the composite likelihood estimator
 #' @param q quantile to be used for the Hill estimator and/or the Heffernan and Tawn conditional extremes model; default set to 0.95
-#' @param k polynomial degree for the Bernstein-B\'ezier polynomials used in the estimation of the angular dependence function using the composite likelihood method; default set to 7
+#' @param k polynomial degree for the Bernstein-Bezier polynomials used in the estimation of the angular dependence function using the composite likelihood method; default set to 7
 #' @param constrained indicates whether or not to incorporate knowledge of the conditional extremes parameters; default set to "no" 
 #' 
 #' @return return curve estimation
 #' 
-#' @details \loadmathjax{} This function estimates the return curve given by 
-#' \mjdeqn{RC(p):=\brace(x, y) \in \mathbb{R}^2: \text{Pr}(X>x, Y>y)=p\brace.} ... talk about how it connects to the estimation of the adf and the methods used, reference the est_lamb function
+#' @details This function estimates the return curve given by \deqn{RC(p):=\brace(x, y) \in \mathbb{R}^2: \text{Pr}(X>x, Y>y)=p\brace.} ... talk about how it connects to the estimation of the adf and the methods used, reference the est_lamb function
 #' 
 #' @rdname returncurve
 #' 
