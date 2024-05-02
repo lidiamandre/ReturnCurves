baf37b59bc21f090130b4f2db523f01fd1bbb690
Andre <andrel@lancaster.ac.uk>
2024-05-02 11:26:23 +0100

    Changes in documentation and NAMESPACE

--- R/returncurve.R
+++ R/returncurve.R
@@ -17,7 +17,7 @@
 #' 
 #' @return return curve estimation
 #' 
-#' @details This function estimates the return curve given by \deqn{RC(p):=\brace(x, y) \in \mathbb{R}^2: \text{Pr}(X>x, Y>y)=p\brace.} ... talk about how it connects to the estimation of the adf and the methods used, reference the est_lamb function
+#' @details \loadmathjax This function estimates the return curve given by \mjdeqn{RC(p):=\brace(x, y) \in \mathbb{R}^2: \text{Pr}(X>x, Y>y)=p\brace.} ... talk about how it connects to the estimation of the adf and the methods used, reference the est_lamb function
 #' 
 #' @rdname returncurve
 #' 
