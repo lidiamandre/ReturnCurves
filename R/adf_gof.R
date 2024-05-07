54b197cd8266dc96b20be78df700625501c97920
lidiamandre <lidiamandre@github.com>
2024-05-07 17:20:07 +0100

    Updated documentation in adf_gof

--- R/adf_gof.R
+++ R/adf_gof.R
@@ -11,13 +11,13 @@
 #' @param w_ind Index of the ray to be considered on the goodness of fit assessment.
 #' @param w Sequence of angles between 0 and 1. Default is \code{seq(0, 1, by = 0.01)}.
 #' @param lambda Vector containing the estimates of the angular dependence function \eqn{\lambda{\omega}}.
-#' @param q Marginal quantile to be used for the min-projection variable \eqn{T} (see *Details*). Default is 0.95.
+#' @param q Marginal quantile to be used for the min-projection variable \eqn{T^*} (see [Details]). Default is 0.95.
 #' @param blocksize Size of the blocks for the block bootstrap procedure. If 1, then a standard bootstrap approach is applied.
 #' @param nboot Number of bootstrap samples to be taken. Default is 250 samples.
 #' @param alpha Significance level to compute the \eqn{(1-\alpha)}% confidence intervals. Default is 0.05.
 #' 
-#' @return Returns a list containing:
-#' \itemize{
+#' 
+#' @return Returns a list containing: \describe{
 #' \item{model}{A vector containing the model quantiles.} 
 #' \item{empirical}{A vector containing the empirical quantiles.}
 #' \item{lower}{A vector containing the lower bound of the confidence interval.}
@@ -25,8 +25,8 @@
 #' }
 #' 
 #' 
-#' @details Define the min projection variable as \eqn{*t*_\omega = t_\omega - u_\omega | t_\omega > u_\omega}.  
-#' Variable \eqn{\lambda(\omega)T_\omega \sim Exp(1)} as \eqn{u_\omega \to \infty} for all \eqn{\omega \in [0,1].} 
+#' @details Define the min projection variable as \eqn{t*_\omega = t_\omega - u_\omega | t_\omega > u_\omega}.  
+#' Variable \eqn{\lambda(\omega)T^*_\omega \sim Exp(1)} as \eqn{u_\omega \to \infty} for all \eqn{\omega \in [0,1].} 
 #' Therefore, a good agreement between the model and empirical quantiles should occur and a the \eqn{y=x} between the quantiles should lie within the \eqn{(1-\alpha)}% confidence band.
 #' The lower and upper bounds of the confidence interval are obtained through bootstrapp of the min-projection variable.
 #'
