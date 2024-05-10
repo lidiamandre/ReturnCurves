8b23214a65b6a9e6ae350a7d57c786d399599551
lidiamandre <lidiamandre@github.com>
2024-05-10 11:46:34 +0100

    Changed est_beta function to account for invalid beta values

--- R/composite_likelihood.R
+++ R/composite_likelihood.R
@@ -14,12 +14,16 @@ est_beta <- function(par, basis, t, len_vec, w, lam_end = c(1, 1)){
   if(any(lam_end > 1)) stop("Value of the ADF is not supported!") # future me: change text!
   beta <- c(lam_end[1], exp(par), lam_end[2])
   lam <- basis %*% beta
-  lam2 <- c() 
-  for(i in 1:length(len_vec)){
-    lam2[(length(lam2) + 1):(length(lam2) + len_vec[i])] <- rep(lam[i], len_vec[i])
+  if(sum(is.infinite(beta)) > 0 | sum(is.na(beta)) > 0){
+    return(1e10)
+  }else{
+    lam2 <- c() 
+    for(i in 1:length(len_vec)){
+      lam2[(length(lam2) + 1):(length(lam2) + len_vec[i])] <- rep(lam[i], len_vec[i])
+    }
+    loglike <- sum(log(lam2)) - sum(lam2 * t)   
+    return(-loglike)
   }
-  loglike <- sum(log(lam2)) - sum(lam2 * t)   
-  return(-loglike)
 }
 
 minfunction_mle <- function(w, data, a = 0, b = 1, lam_end = c(1, 1), k = 7, q = 0.95, tol = 0.0001){
