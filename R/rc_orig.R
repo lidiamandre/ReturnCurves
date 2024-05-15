88e96f61aee4c770106b3272d302363c56dc0e9d
lidiamandre <lidiamandre@gmail.com>
2024-05-15 14:27:37 +0100

    Fixed error in rc_o

--- R/rc_orig.R
+++ R/rc_orig.R
@@ -6,7 +6,7 @@ rc_est <- function(data, w, p, method, q_minproj, qalphas, k, constrained, tol){
   xp <- qexp(1 - p)
   lambda <- adf_est(data = data, w = w, method = method, q = q_minproj, qalphas = qalphas, k = k, constrained = constrained, tol = tol)
   thresh <- sapply(w, function(i) minproj_lambda(data, i, q_minproj = q_minproj)$thresh)
-  r <- sapply(1:n, function(i) thresh[i] - log(p/(1 - q))/lambda[i])
+  r <- sapply(1:n, function(i) thresh[i] - log(p/(1 - q_minproj))/lambda[i])
   x <- sapply(1:n, function(i) r[i] * w[i])
   y <- sapply(1:n, function(i) r[i] * (1 - w[i]))
   for(i in 1:length(x)){
